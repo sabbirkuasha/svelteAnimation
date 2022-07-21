07a47ca6349ff248662d09c83a050cbc9e44c282
sabbirz <sabbir.kuasha@gmail.com>
2022-07-22 01:57:56 +0600

    svelte basic motion code
    basic svelte motion code

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,28 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import {writable} from 'svelte/store'
+  import {tweened} from 'svelte/motion'
+  import {cubicIn, elasticIn, elasticOut, expoInOut, quadOut, quintIn} from 'svelte/easing'
+
+  // for different easing function visit this url
+  // https://svelte.dev/repl/6904f0306d6f4985b55f5f9673f762ef?version=3.4.1
+  const progress = tweened(0,{
+    delay:0,
+    duration: 700,
+    easing: elasticOut
+  })
+
+
+  setTimeout(()=>{
+    progress.set(.75)
+  },1500)
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <progress value={$progress}>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  </progress>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
